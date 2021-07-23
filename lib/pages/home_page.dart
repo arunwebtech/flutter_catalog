ee923d1d7581c0f101b97f13e78127c44e31cebf
swaggy_arun <arunj1408@gmail.com>
2021-07-23 17:59:14 +0530

    list commit
    Signed-off-by: swaggy_arun <arunj1408@gmail.com>

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,24 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
+import 'package:flutter_catalog/widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(4, (index) => CatalogModel.items[0]);
+
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: Text("Catalog App"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-          ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
         ),
       ),
       drawer: MyDrawer(),
